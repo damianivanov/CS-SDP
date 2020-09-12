5de95c32bf2b63012d6d8b723cd873d43ea0291c
Damian <damian.iv@abv.bg>
2020-09-12 22:49:39 +0300

    Insert_at/remove_at/DOCTEST

--- Structures/SLL/LinkedList.cpp
+++ Structures/SLL/LinkedList.cpp
@@ -1,15 +1,27 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest.h"
 #include <iostream>;
 #include "LinkedList.h";
-using namespace std;
+
+//Basic Insertion
+TEST_CASE("Insertion Test")
+{
+    LinkedList<int> LL; 
+    LL.add_element(1);
+    LL.add_element(2);
+    CHECK(LL.length() == 2);
+    LL.add_element(3);
+    LL.add_element(4);
+    CHECK(LL.length() == 4);
+
+    CHECK(LL.get_element_at(0) == 1);
+    CHECK(LL.get_element_at(1) == 2);
+    CHECK(LL.get_element_at(2) == 3);
+    CHECK(LL.get_element_at(3) == 4);
+}
+
 
 int main()
 {
-	LinkedList<int> LL =  LinkedList<int>();
-	LL.add_Element(2);
-	LL.add_Element(3);
-	LL.add_Element(4);
-	LL.add_Element(5);
-	LL.add_Element(6);
-	LL.add_Element(7);
-	LL.Print();
+	doctest::Context().run();
 }
--- Structures/SLL/LinkedList.h
+++ Structures/SLL/LinkedList.h
@@ -20,15 +20,15 @@ class LinkedList
 {
 private:
     Node<T> *head;
-    int _size;
+    int size;
     Node<T> *tail;
 
 public:
-    int get_size() { return this._size; }
+    int length() { return this->size; }
     Node<T> *get_Head() { return this->head; }
     Node<T> *get_Tail() { return this->tail; }
 
-    void add_Element(T element)
+    void add_element(T element)
     {
         if (!head)
         {
@@ -36,7 +36,7 @@ public:
             new_head->next = nullptr;
             this->head = new_head;
             this->tail = new_head;
-            _size++;
+            size++;
             BOOST_LOG_TRIVIAL(info) << "Head was added to the Linked List";
             BOOST_LOG_TRIVIAL(info) << "New element was added to the Linked List";
             BOOST_LOG_TRIVIAL(info) << "Tail was added to the Linked List";
@@ -48,15 +48,56 @@ public:
             this->tail->next = new_node;
             this->tail = new_node;
             this->tail->next = nullptr;
-            _size++;
+            size++;
             BOOST_LOG_TRIVIAL(info) << "New element was added to the Linked List";
         }
     }
-
-    void Print()
+    void remove_at(int index)
+    {
+		if (index >= _size)
+        {
+            BOOST_LOG_TRIVIAL(info) << "Invalid index";
+        }
+        Node<T>* currNode=head;
+        for (size_t i = 0; i < index-1; i++)
+        {
+            currNode = currNode->next;
+        }
+        currNode->next = currNode->next->next;
+        BOOST_LOG_TRIVIAL(info) << "Removed element from Linked List";
+    }
+    void insert_at(int index, T element)
+    {
+        Node<T>* newNode = new Node<T>(element);
+        if (index == _size - 1)
+        {
+            Node<T>* curr = tail;
+            tail->next = newNode;
+            newNode->next = nullptr;
+        }
+        else {
+            Node<T>* curr = head;
+            for (size_t i = 0; i < index - 1; i++)
+            {
+                curr = curr->next;
+            }
+            newNode->next = curr->next;
+            curr->next = newNode;
+        }
+    }
+    T get_element_at(int index) {
+        Node<T>* curr = head;
+        for (size_t i = 0; i < index; i++)
+        {
+            curr = curr->next;
+        }
+        return curr->data;
+        
+    }
+    void print()
     {
         Node<T>* curr_node = head;
-        while (curr_node!=NULL)
+        while (curr_node!=nullptr)
         {
             std::cout << curr_node->data << endl;
             curr_node = curr_node->next;
@@ -66,7 +107,7 @@ public:
     LinkedList() {
         this->head = nullptr;
         this->tail = nullptr;
-        this->_size = 0;
+        this->size = 0;
     }
 
     ~LinkedList() {};
